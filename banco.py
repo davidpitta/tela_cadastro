698a9a57036337cd47ed3a6557e165fae6427e1e
David Pitta <davidfrpitta@gmail.com>
2022-11-17 11:01:16 -0300

    Adicionando gitignore

--- banco.py
+++ banco.py
@@ -1,5 +1,13 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-cnx = mysql.connector.connect(host='localhost', user='root', password='12345678',database='cadastro')
+user = os.environ.get('user')
+password = os.environ.get('password')
+host = os.environ.get('host')
+database = os.environ.get('database')
+
+cnx = mysql.connector.connect(user=user, password=password, host=host, database=database)
 
 cnx.close()
